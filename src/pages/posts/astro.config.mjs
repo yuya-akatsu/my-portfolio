e177685ee38c4e45ca7d17ff8b80fc6184e67ca7
yuya-akatsu <yuyaakatsu@gmail.com>
2024-10-23 16:07:37 +0900

    jsxの追加

--- src/pages/posts/astro.config.mjs
+++ src/pages/posts/astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
-
-import netlify from '@astrojs/netlify';
-
-
-
+import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify()
-
+  integrations: [preact()]
 });
