c9f6fa356dfc6ab176c4c989f6ff7b5a30a59ae2
yuya-akatsu <yuyaakatsu@gmail.com>
2024-10-25 11:24:25 +0900

    react追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import netlify from "@astrojs/netlify";
 
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
 });
