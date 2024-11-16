d1c4c15369ac3896765dc61e6759abe29148cdeb
yuya-akatsu <yuyaakatsu@gmail.com>
2024-11-16 11:53:15 +0900

    buildファイルのコミット

---
+++ .netlify/build/chunks/BaseLayout_I9GrWUy3.mjs
@@ -0,0 +1,30 @@
+import { c as createComponent, a as createAstro, r as renderTemplate, m as maybeRenderHead, b as addAttribute, e as renderHead, d as renderComponent, f as renderSlot } from './astro/server_Bvyz7Nc1.mjs';
+import 'kleur/colors';
+import 'clsx';
+/* empty css                            */
+
+const $$Astro$1 = createAstro();
+const $$Header = createComponent(($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$1, $$props, $$slots);
+  Astro2.self = $$Header;
+  return renderTemplate`${maybeRenderHead()}<header class="bg-white mb-10 fixed top-0 left-0 right-0 shadow z-50" data-astro-cid-3ef6ksr2> <div class="max-w-5xl container mx-auto px-4 py-4 flex justify-between items-center" data-astro-cid-3ef6ksr2> <a href="/" data-astro-cid-3ef6ksr2> <div class="text-xl font-bold text-gray-800" data-astro-cid-3ef6ksr2>
+Yuya Akatsu
+</div> </a> ${Astro2.url.pathname === "/" ? renderTemplate`<nav class="space-x-4 hidden md:block" data-astro-cid-3ef6ksr2> <a href="#works" class="capitalize  relative pb-1 line" data-astro-cid-3ef6ksr2>works</a> <a href="#service" class="capitalize relative pb-1 line" data-astro-cid-3ef6ksr2>service</a> <a href="#profile" class="capitalize relative pb-1 line" data-astro-cid-3ef6ksr2>profile</a> <a href="#contact" class="capitalize relative pb-1 line" data-astro-cid-3ef6ksr2>contact</a> </nav>` : null} </div> </header> `;
+}, "/Users/yuyaakatsu/my-portfolio/src/components/Header.astro", void 0);
+
+const $$Footer = createComponent(($$result, $$props, $$slots) => {
+  const currentYear = (/* @__PURE__ */ new Date()).getFullYear();
+  return renderTemplate`${maybeRenderHead()}<footer class="bg-black text-white py-4"> <div class="container mx-auto px-4"> <div class="flex justify-center items-center"> <p class="text-sm">
+&copy; ${currentYear} Yuya Akatsu.
+</p> </div> </div> </footer>`;
+}, "/Users/yuyaakatsu/my-portfolio/src/components/Footer.astro", void 0);
+
+const $$Astro = createAstro();
+const $$BaseLayout = createComponent(($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$BaseLayout;
+  const { pageTitle } = Astro2.props;
+  return renderTemplate`<html lang="ja"> <head><meta charset="utf-8"><link rel="icon" type="image/jpg" href="/public/mainVisualImage.jpg"><meta name="viewport" content="width=device-width"><meta name="generator"${addAttribute(Astro2.generator, "content")}><title>Yuya Akatsu | Web Designer</title>${renderHead()}</head> <body class="bg-gray-100 leading-relaxed"> ${renderComponent($$result, "Header", $$Header, {})} <main> ${renderSlot($$result, $$slots["default"])} </main> ${renderComponent($$result, "Footer", $$Footer, {})} </body></html>`;
+}, "/Users/yuyaakatsu/my-portfolio/src/layouts/BaseLayout.astro", void 0);
+
+export { $$BaseLayout as $ };
---
+++ .netlify/v1/functions/ssr/.netlify/build/entry.mjs
@@ -0,0 +1,32 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_B1ktJIvN.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/_blogid_.astro.mjs');
+const _page2 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/[blogId].astro", _page1],
+    ["src/pages/index.astro", _page2]
+]);
+const serverIslandMap = new Map();
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "ef04a011-a5f3-4e3f-93be-5e71d7255da4"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
