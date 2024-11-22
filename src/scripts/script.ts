4dd26eb35b4fc0e738563e6b16c3b8a6027e19d6
yuyaakatsu <yuyaakatsu@gmail.com>
2024-11-22 10:22:29 +0900

    [update]npmでスクロールトリガーの追加

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -13,47 +13,41 @@ document.querySelectorAll('nav a').forEach((anchor: HTMLAnchorElement) => {
   });
 });
 import {gsap} from "gsap";
-const tween = gsap.to("header",{
-duration:0.5,
-paused:true,
-ease:"power2.out",
-top:0,
-});
-document.querySelector("header").addEventListener("click",tween.play())
-import {gsap} from "gsap";
-gsap.registerPlugin(ScrollTrigger);
-gsap
-  .timeline({
-    defaults:{
-      ease:"power2.out",
-      duration:1,
-    },
-    scrollTrigger:{
-      markers:true,
-      trigger:".content",
-      start:"top 50%",
-      end:"bottom top",
-      toggleActions:"restart none none none",
-    },
-  })
-  import {gsap} from "gsap";
+
 	gsap.to("#mainVisualImage",{
 		repeat:-1,
-
+    rotate:360,
 		duration:2,
+    ease:'none',
 	})
-	gsap.set(".listItem",{
-		y:20,
-		opacity:0,
-	});
-gsap.to('.listItem', 1.5, {
-  y: 0,
-  opacity: 1,
-  ease: 'expo.out',
-  stagger: {
-    each: .1,
-    // amount: 1 // 処理する個数 / amountの値
-    from: 'start'
-      // start, end, center, edges, random
-  }
+// カードがスクロールに合わせて出現
+document.querySelectorAll(".listItem").forEach((el) => {
+  gsap.fromTo(
+    el,
+    { y: 50, opacity: 0 },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 1.5,
+      scrollTrigger: {
+        trigger: el,
+        start: "top 90%",
+        ease: "expo",
+      },
+    }
+  );
 });
+// 	gsap.set(".listItem",{
+// 		y:20,
+// 		opacity:0,
+// 	});
+// gsap.to('.listItem', 1.5, {
+//   y: 0,
+//   opacity: 1,
+//   ease: 'expo.out',
+//   stagger: {
+//     each: 0.8,
+
+//     from: 'start'
+//   }
+// });
