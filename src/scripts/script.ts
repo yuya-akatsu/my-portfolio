597d68e845a1be52f7bb7130fa40f31b51aa2d31
yuyaakatsu <yuyaakatsu@gmail.com>
2024-11-22 09:03:19 +0900

    [fix]
    インデックスに記述したscriptをtsファイルに移植

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -12,3 +12,48 @@ document.querySelectorAll('nav a').forEach((anchor: HTMLAnchorElement) => {
     }
   });
 });
+import {gsap} from "gsap";
+const tween = gsap.to("header",{
+duration:0.5,
+paused:true,
+ease:"power2.out",
+top:0,
+});
+document.querySelector("header").addEventListener("click",tween.play())
+import {gsap} from "gsap";
+gsap.registerPlugin(ScrollTrigger);
+gsap
+  .timeline({
+    defaults:{
+      ease:"power2.out",
+      duration:1,
+    },
+    scrollTrigger:{
+      markers:true,
+      trigger:".content",
+      start:"top 50%",
+      end:"bottom top",
+      toggleActions:"restart none none none",
+    },
+  })
+  import {gsap} from "gsap";
+	gsap.to("#mainVisualImage",{
+		repeat:-1,
+
+		duration:2,
+	})
+	gsap.set(".listItem",{
+		y:20,
+		opacity:0,
+	});
+gsap.to('.listItem', 1.5, {
+  y: 0,
+  opacity: 1,
+  ease: 'expo.out',
+  stagger: {
+    each: .1,
+    // amount: 1 // 処理する個数 / amountの値
+    from: 'start'
+      // start, end, center, edges, random
+  }
+});
